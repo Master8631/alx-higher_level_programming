bc47a8bb3e5874f5497cf290da3d43fe8a5bae5b
Master8631 <murigipeternjogu254@gmail.com>
2023-06-08 00:39:49 +0000

    task5c

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(99):
-    for j in range(i + 1, 10):
-        if i * 10 + j < 89:
-            print("{:d}{:d}".format(i,j), end=",")
-print("{:d}".format(89))
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=",")

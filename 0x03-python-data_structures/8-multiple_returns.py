ca3d85613a62eddb0109d3cbc2e66140d9ea4565
Master8631 <murigipeternjogu254@gmail.com>
2023-06-13 11:52:46 +0000

    task 8f

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,5 @@
 def mutliple_returns(sentence):
     """returns the length of sentence and its first character as a tuple"""
 
-    if sentence == "":
-        return (0, None)
-    return (len(sentence), sentence[0])
+    res = (len(sentence), sentence[0] if sentence else "None")
+    return res

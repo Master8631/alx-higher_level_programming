09005798113cd44083e2f68c64665886613624b4
Master8631 <murigipeternjogu254@gmail.com>
2023-07-04 20:03:25 +0000

    task30

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module defines the LockedClass class."""
+
+class LockedClass():
+    """This class prevents dynamic creation of most instance attributes.
+
+    It prevents the dynamic creation of attributes that are not called
+    first_name.
+    """
+
+    __slots__ = "first_name"
